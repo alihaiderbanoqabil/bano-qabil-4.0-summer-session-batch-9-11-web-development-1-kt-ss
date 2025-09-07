bf59d590998a854e87beb9fc4bb017bc094e91eb
alihaiderbanoqabil <alihaiderbanoqabil2.0@gmail.com>
2025-09-07 11:30:17 +0500

    done with class2 of js

---
+++ class17/script.js
@@ -0,0 +1,117 @@
+"use strict";
+
+// var number1;
+// var number2;
+var fullName = "ali haider";
+// console.log(fullName);
+// apart from $ and _ we cant use any other special character in variable name
+var fullname = "ali haider";
+var full_name = "ali haider";
+var fullName = "ali haider";
+var FullName = "ali haider";
+
+var fullname = "ali haider",
+  full_name = "ali haider",
+  fullName = "ali haider",
+  FullName = "ali haider";
+
+// var a;
+// var b;
+// var c;
+// let d;
+// // OR
+// var a, b, c, d;
+// let d;
+
+// var a = 10;
+// console.log("a:", a);
+
+// var b = 100;
+// console.log("b:", b);
+
+// var age;
+// var isAdult = true;
+// if (isAdult) {
+//   age = 20;
+// } else {
+//   age = 15;
+// }
+// console.log(age);
+
+// //here i store ali haider in a variable
+// var a = "ali haider";
+// console.log(a);
+
+// data types in js
+
+// var name = "ali haider"; //string
+// var name = "20"; //string
+// var age = 20; //number
+// var isAdult = true; //boolean
+// var isAdult = "true"; //boolean
+// var isStudent = false; //boolean
+// var address = undefined; //undefined
+// var address; //undefined
+// console.log("address 1:", address);
+// address = "HOUSE NO 32"; // string
+// // console.log("address 2:", Address);
+// console.log("address 2:", address);
+
+// var phone = null; //null
+// console.log("phone:", phone);
+// var longNumber = 123456789012345678901234567890
+// console.log("longNumber:", longNumber);
+
+// // var n1 = 10;
+// var n1 = "10";
+// var n2 = 20;
+// var sum = n1 + n2;
+// console.log("sum:", sum);
+
+// console.log("number1", number1);
+// console.log("number2", number2);
+
+// var number1;
+// var number2 = 10;
+
+// console.log("number1", number1);
+// console.log("number2", number2);
+
+// console.log(10 + 20); //30
+// console.log("10" + 20); //"1020"
+// console.log(10 + "20"); //"1020"
+// console.log("10" + "20"); //"1020"
+// console.log("Hello " + "World"); //"Hello World"
+// console.log("Hello " + 20); //"Hello 20"
+// console.log(10 + " World"); //"10 World"
+// console.log("Hello " + true); //"Hello true"
+// console.log("Hello " + false); //"Hello false"
+// console.log("Hello " + null); //"Hello null"
+// console.log("Hello " + undefined); //"Hello undefined"
+// console.log("Hello " + NaN); //"Hello NaN"
+// console.log("Hello " + 10 + 20); //"Hello 1020"
+// console.log("Hello " + (10 + 20)); //"Hello 30"
+// console.log(10 + 20 + " Hello ");
+// console.log(20 * "20"); //400
+// console.log(20 * "abc"); //NaN
+// console.log(20 / "abc"); //NaN
+// console.log(20 / "2"); //10
+// console.log(20 - "2"); //18
+// console.log(20 - "abc"); //NaN
+
+// type conversion / type casting in js 
+var n1 = "10";
+var n2 = 20;
+var sum = Number(n1) + n2;
+console.log("sum:", sum);
+
+var num1 = "20.5";
+var number1 = Number(num1);
+// console.log("num1:", num1);
+// console.log("num1 type:", typeof num1);
+// console.log("number1:", number1);
+// console.log("number1 type:", typeof number1);
+
+console.log("num1 type:", typeof true);
+console.log("num1 type:", typeof  b);
+console.log("num1 type:", typeof undefined);
